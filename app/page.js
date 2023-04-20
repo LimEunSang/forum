bbda1ed240cc06e9960198b9642592029eaffa54
LimEunSang <dmstkd2905@naver.com>
2023-04-20 15:50:03 +0900

    ~ Next.js에서 MongoDB 사용하기
    - MongoDB 라이브러리 설치
    - MongoDB 사용 환경 셋팅

--- app/page.js
+++ app/page.js
@@ -1,3 +1,10 @@
-export default function Home() {
-  return <></>;
+import { connectDB } from "@/util/database";
+
+export default async function Home() {
+  const client = await connectDB;
+  const db = client.db("forum");
+  let result = await db.collection("post").find().toArray();
+  console.log(result);
+
+  return <>안녕</>;
 }
