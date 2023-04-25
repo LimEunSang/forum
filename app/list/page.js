c1123790c8dd6342bbdb37d4cb82ae395e74813d
LimEunSang <dmstkd2905@naver.com>
2023-04-25 21:35:06 +0900

    ~ static rendering, dynamic rendering, cache
    build 시
    list page - static rendering 되는 옵션을 dynamic rendering으로 변경

--- app/list/page.js
+++ app/list/page.js
@@ -1,6 +1,8 @@
 import { connectDB } from "@/util/database";
 import ListItem from "./ListItem";
 
+export const dynamic = "force-dynamic";
+
 export default async function List() {
   const client = await connectDB;
   const db = client.db("forum");
