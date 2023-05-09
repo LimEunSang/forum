008a0006ccc819df225c8ae03f3e09500df3dac0
LimEunSang <dmstkd2905@naver.com>
2023-05-09 20:42:16 +0900

    하나의 함수 내에서 이미지 서버 등록과 글 등록 기능 동시에 실행 불가능 문제 해결
    - setImgURL 함수가 늦게 완료되는 문제로 인해 작성 버튼을 2번 눌러야
      정상적으로 이미지 경로를 서버로 전송하는 문제 해결
    - useEffect를 사용하여 imgURL 변경 시 글 등록 함수를 자동으로 수행하는 로직으로 구현

--- app/write/WriteForm.js
+++ app/write/WriteForm.js
@@ -1,6 +1,6 @@
 "use client";
 
-import { useState } from "react";
+import { useState, useEffect } from "react";
 
 export default function WriteForm() {
   const [title, setTitle] = useState("");
@@ -9,9 +9,22 @@ export default function WriteForm() {
   const [imgURL, setImgURL] = useState(""); // img 저장 서버 경로
   const [file, setFile] = useState(null);
 
-  const handleSubmit = async (event) => {
-    event.preventDefault();
+  const assignPost = () => {
+    fetch("/api/post/new", {
+      method: "POST",
+      body: JSON.stringify({
+        title: title,
+        content: content,
+        imgURL: imgURL,
+      }),
+    })
+      .then((response) => {
+        if (response.status == 200) window.location.replace("/list");
+      })
+      .catch((error) => alert(error));
+  };
 
+  const handleSubmit = async () => {
     // 파일 업로드
     if (file) {
       let filename = encodeURIComponent(file.name);
@@ -29,32 +42,19 @@ export default function WriteForm() {
 
       if (res.ok) {
         setImgURL(res.url + "/" + filename);
-        // 비동기 함수의 실행 속도로 인하여 하단 글 작성 비동기 코드가 늦게 실행
-        // 따라서 작성 버튼 한번 클릭 시 이미지를 업로드하고 두 번째 눌러야 글 작성
-        if (!imgURL)
-          alert(
-            "서버에 이미지가 업로드 되었습니다. 다시 한번 작성 버튼을 눌러주세요."
-          );
+        // 이 자리에 assignPost()를 넣으면 setImgURL()가 완료되기 전에 실행됨
       } else {
         console.log("이미지 서버 업로드 실패");
       }
     }
 
-    // 글 작성
-    if (!file || imgURL)
-      fetch("/api/post/new", {
-        method: "POST",
-        body: JSON.stringify({
-          title: title,
-          content: content,
-          imgURL: imgURL,
-        }),
-      })
-        .then((response) => {
-          if (response.status == 200) window.location.replace("/list");
-        })
-        .catch((error) => alert(error));
+    if (!file) assignPost();
   };
+
+  // setImgURL()가 정상적으로 완료된 이후에 assignPost() 실행
+  useEffect(() => {
+    assignPost();
+  }, [imgURL]);
 
   return (
     <div className="p-20">
