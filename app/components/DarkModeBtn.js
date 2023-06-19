0fda765f660e019c9ffc7f75a055f93fb5f600a6
LimEunSang <dmstkd2905@naver.com>
2023-06-19 10:04:37 +0900

    darkmode 적용되지 않는 버그 해결
    - 생성되는 cookie의 path값을 명시하여 해결
    - 자세한 내용은 issue 내용 확인
    
    [#9]

--- app/components/DarkModeBtn.js
+++ app/components/DarkModeBtn.js
@@ -6,15 +6,19 @@ import { useEffect } from "react";
 export default function DarkModeBtn({ mode }) {
   let router = useRouter();
 
+  console.log(mode);
+
   const modeBtnHandler = () => {
     const opposite = mode === "dark" ? "light" : "dark";
-    document.cookie = `mode=${opposite}; max-age=` + 3600 * 24 * 400;
+    console.log(opposite);
+    document.cookie =
+      `mode=${opposite}; max-age=` + 3600 * 24 * 400 + "; path=/";
     router.refresh();
   };
 
   useEffect(() => {
     if (!mode) {
-      document.cookie = "mode=light; max-age=" + 3600 * 24 * 400;
+      document.cookie = "mode=light; max-age=" + 3600 * 24 * 400 + "; path=/";
     }
   }, []);
 
