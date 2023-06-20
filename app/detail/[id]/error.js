8ab3ad5147e10535c9796e18d5df219bf30f92ca
LimEunSang <dmstkd2905@naver.com>
2023-06-20 13:07:10 +0900

    CSS - 404, error page
    - 배경 이미지 적용
    - 출처 표시 완-벽
    
    [#3]

--- app/detail/[id]/error.js
+++ app/detail/[id]/error.js
@@ -1,16 +1,24 @@
 "use client";
 
+/* 서버가 다운되거나 DB 데이터 조회에 실패할 때 나타나는 페이지 */
+/* 에러 메시지 출력: props.error */
 export default function Error({ error, reset }) {
   return (
-    <>
-      <h4>error..!!</h4>
+    <div className="pageError">
+      <div className="source">
+        <a href="https://kr.freepik.com/free-vector/tiny-people-examining-operating-system-error-warning-on-web-page-isolated-flat-illustration_11235921.htm#query=error%20page&position=2&from_view=search&track=ais">
+          작가 pch.vector
+        </a>
+        <a href="https://kr.freepik.com/">출처 Freepik</a>
+      </div>
       <button
+        className="commonBtn"
         onClick={() => {
           reset();
         }}
       >
         reset
       </button>
-    </>
+    </div>
   );
 }
--- app/detail/[id]/not-found.js
+++ app/detail/[id]/not-found.js
@@ -1,3 +1,18 @@
+import Link from "next/link";
+
+/* 존재하지 않는 페이지에 접근했을 때 나타나는 페이지 */
 export default function NotFound() {
-  return <h4>404 없는 페이지입니다</h4>;
+  return (
+    <div className="page404">
+      <div className="source">
+        <a href="https://kr.freepik.com/free-vector/404-error-with-a-landscape-concept-illustration_20602785.htm#query=404%20error%20page&position=4&from_view=keyword&track=ais">
+          작가 storyset
+        </a>
+        <a href="https://kr.freepik.com/">출처 Freepik</a>
+      </div>
+      <Link className="commonBtn HomeBtn" href="/">
+        홈으로
+      </Link>
+    </div>
+  );
 }
