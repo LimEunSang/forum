4f8e0d5be009be3b6ab24b522eb3f59ca681e65b
LimEunSang <dmstkd2905@naver.com>
2023-06-20 11:28:48 +0900

    CSS - 회원가입 페이지
    (쿠팡 홈페이지를 매우 많이 참고)

--- app/register/page.js
+++ app/register/page.js
@@ -1,11 +1,40 @@
 export default function Register() {
   return (
-    <div>
+    <div className="register">
+      <span>회원정보를 입력해주세요</span>
       <form method="POST" action="/api/auth/signup">
-        <input name="name" type="text" placeholder="이름" />
-        <input name="email" type="text" placeholder="이메일" />
-        <input name="password" type="password" placeholder="비밀번호" />
-        <button type="submit">id/pw 가입요청</button>
+        <div className="inputField">
+          <div className="iconBox">👤</div>
+          <input
+            className="nameInput"
+            name="name"
+            type="text"
+            placeholder="이름"
+          />
+        </div>
+        <div className="inputField">
+          <div className="iconBox">✉️</div>
+          <input
+            className="emailInput"
+            name="email"
+            type="text"
+            placeholder="이메일"
+          />
+        </div>
+        <div className="inputField">
+          <div className="iconBox">🔒</div>
+          <input
+            className="passwordInput"
+            name="password"
+            type="password"
+            placeholder="비밀번호"
+          />
+        </div>
+        <div className="btnWrapper">
+          <button className="commonBtn" type="submit">
+            회원가입
+          </button>
+        </div>
       </form>
     </div>
   );
