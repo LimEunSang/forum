388a517db18ca7848d10d4922efb3151f2a6182c
LimEunSang <dmstkd2905@naver.com>
2023-07-13 09:03:40 +0900

    코드 리펙토링
    post list 요청 코드를 main page -> PostList component로 이동

--- app/components/PostList.js
+++ app/components/PostList.js
@@ -1,10 +1,27 @@
+import { connectDB } from "@/util/database";
+import { getServerSession } from "next-auth";
+import { authOptions } from "@/pages/api/auth/[...nextauth]";
 import PostItem from "./PostItem";
 
-const PostList = ({ postList, user }) => {
+const PostList = async () => {
+  const session = await getServerSession(authOptions);
+
+  const client = await connectDB;
+  const db = client.db("forum");
+  let result = await db.collection("post").find().toArray();
+  result = result.reverse(); // 최신 게시물을 앞에 위치
+
+  /* server에서 client로 ObjectId 자료형 객체를 전달할 때 경고문 발생
+     → string 자료형으로 변환하여 전달 */
+  result = result.map((object) => {
+    object._id = object._id.toString();
+    return object;
+  });
+
   return (
     <div className="postList">
-      {postList.map((object, key) => (
-        <PostItem object={object} key={key} user={user} />
+      {result.map((object, key) => (
+        <PostItem object={object} key={key} user={session && session.user} />
       ))}
     </div>
   );
